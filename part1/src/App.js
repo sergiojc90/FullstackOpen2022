cbe3922b4218bb63fae414fbdb5de77e554dd4ba
Ricardo Juárez <rjuarez@augustasportswear.com>
2022-06-21 00:21:43 -0500

    Rebuilding index.js

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,33 +1,27 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-const Hello = (props) =>{
-  return(
+  return (
     <div>
+      <h1>{course}</h1>
       <p>
-        Hello World {props.name}, you are {props.age} years old
+        {part1} {exercises1}
       </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
-const App = () =>{
-  const now = new Date()
-  const a = 10
-  const b = 20
-  const age = 34  
-
-  return(
-    <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
-      <Hello name = "Rich" age = {21+10}/>
-      <Hello name = "Emma" age = {1}/>
-      <Hello name = "Wendy" age = {age}/>
-    </div>
-
-  )
-}
-
-
-export default App;
+export default App
