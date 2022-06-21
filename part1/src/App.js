a833814c35fdfa33708be5eaa7ff069707daa779
Ricardo Juárez <rjuarez@augustasportswear.com>
2022-06-21 00:05:17 -0500

    Using multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,33 @@
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Hello = (props) =>{
+  return(
+    <div>
+      <p>
+        Hello World {props.name}, you are {props.age} years old
+      </p>
     </div>
-  );
+  )
 }
+
+const App = () =>{
+  const now = new Date()
+  const a = 10
+  const b = 20
+  const age = 34  
+
+  return(
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
+      <Hello name = "Rich" age = {21+10}/>
+      <Hello name = "Emma" age = {1}/>
+      <Hello name = "Wendy" age = {age}/>
+    </div>
+
+  )
+}
+
 
 export default App;
