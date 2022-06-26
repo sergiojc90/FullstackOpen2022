04b3c4f00e52614ca05207ea4709cfdbcd998a7d
Ricardo <sergiojc90@gmail.com>
2022-06-26 11:50:33 -0500

    Debugging the .join issue, we were not using an array as the initial state

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react"
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () =>{
     setAll(allClicks.concat("L"))
@@ -17,10 +17,11 @@ const App = () => {
 
   return(
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}> left </button>
       <button onClick={handleRightClick}> right </button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   )
 }
