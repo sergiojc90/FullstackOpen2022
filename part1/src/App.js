f4187e9431b521a13f49859b6e3958d8b583c1fe
Ricardo Juárez <rjuarez@augustasportswear.com>
2022-06-26 10:46:32 -0500

    Clicks application, passing a objets as the initial state for the useState funciton

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,65 +1,38 @@
-const Header = (props) =>{
-  return(
-    <h1>
-      {props.course}
-    </h1>
-  )
-}
-
-const Part = (props) =>{
-  return(
-
-    <p>
-    {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) =>{
-  return(
-    <div>
-
-      <Part part = {props.parts[0].name} exercises = {props.parts[0].exercises}/>
-      <Part part = {props.parts[1].name} exercises = {props.parts[1].exercises}/>
-      <Part part = {props.parts[2].name} exercises = {props.parts[2].exercises}/>
-
-
-    </div>
-  )
-}
-
-const Total = (props) =>{
-  return(
-    <p>
-      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  )
-}
+import { useState } from "react"
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const[clicks, setClicks] = useState({
+    left:0, right:0
+  })
+
+  const handleLeftClick = () =>{
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+
+    setClicks(newClicks)
   }
 
-  return (
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right +1
+    }
+
+    setClicks(newClicks)
+  }
+
+  return(
     <div>
-      <Header course = {course.name}/>
-      <Content parts = {course.parts}/>
-      <Total parts = {course.parts}/>
+      {clicks.left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {clicks.right}
     </div>
   )
 }
