db98576ee16d70f734344f1c1d70192337f39793
Ricardo <sergiojc90@gmail.com>
2022-06-28 00:31:36 -0500

    Adding states to track total count, positive count and score for each feedback

--- part1/src/App_1.6_1.14.js
+++ part1/src/App_1.6_1.14.js
@@ -8,12 +8,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [count, setCount] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [score, setScore] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
+  const handleGood = () => {
+    setCount(count + 1)
+    setPositive(positive + 1)
+    setScore(score + +1)
+    setGood(good + 1)
+  }
 
-  const handleNeutral = () => setNeutral(neutral + 1)
-
-  const handleBad = () => setBad(bad + 1)
+  const handleNeutral = () => {
+    setCount(count + 1)
+    setNeutral(neutral + 1)
+  }
+  
+  const handleBad = () => {
+    setCount(count + 1)
+    setScore(score - 1)
+    setBad(bad + 1)
+  }
 
   return (
     <div>
@@ -26,6 +41,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {count}</p>
+      <p>Average {score/count}</p>
+      <p>Positive {positive/count*100}%</p>
     </div>
   )
 }
