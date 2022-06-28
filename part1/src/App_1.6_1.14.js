9d01e20e24e3401a78ed44580b1f4863024fe214
Ricardo <sergiojc90@gmail.com>
2022-06-28 00:55:42 -0500

    1.7 Refactoring the statistics component

--- part1/src/App_1.6_1.14.js
+++ part1/src/App_1.6_1.14.js
@@ -3,18 +3,30 @@ import { useState } from 'react'
 
 const Button = ({handleClick,text}) => <button onClick={handleClick}>{text}</button>
 
+const Statistics = ({good, neutral, bad, count, score}) =>{
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {count}</p>
+      <p>Average {score/count}</p>
+      <p>Positive {good/count*100}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [count, setCount] = useState(0)
-  const [positive, setPositive] = useState(0)
   const [score, setScore] = useState(0)
 
   const handleGood = () => {
     setCount(count + 1)
-    setPositive(positive + 1)
     setScore(score + +1)
     setGood(good + 1)
   }
@@ -37,13 +49,8 @@ const App = () => {
       <Button handleClick={handleNeutral} text = 'Neutral'/>
       <Button handleClick={handleBad} text = 'Bad'/>
 
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {count}</p>
-      <p>Average {score/count}</p>
-      <p>Positive {positive/count*100}%</p>
+      <Statistics good ={good} neutral ={neutral} bad ={bad} count ={count} score ={score} />
+
     </div>
   )
 }
