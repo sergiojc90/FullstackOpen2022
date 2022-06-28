1a3723576cf4fa22f5ef83bc0152378ee07bb38a
Ricardo <sergiojc90@gmail.com>
2022-06-28 00:12:56 -0500

    1.6 creating buttons and event handlers on click for each feedback

--- part1/src/App_1.6_1.14.js
+++ part1/src/App_1.6_1.14.js
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 
+
+const Button = ({handleClick,text}) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+
+  const handleNeutral = () => setNeutral(neutral + 1)
+
+  const handleBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGood} text = 'Good'/>
+      <Button handleClick={handleNeutral} text = 'Neutral'/>
+      <Button handleClick={handleBad} text = 'Bad'/>
+
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
