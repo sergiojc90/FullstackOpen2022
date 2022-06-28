9dffc85f4215a756c7ce3be3a223c7094e8cebe8
Ricardo <sergiojc90@gmail.com>
2022-06-27 23:58:54 -0500

    Creating starting point code for exercises 1.6 - 1.14

--- part1/src/App_1.6_1.14.js
+++ part1/src/App_1.6_1.14.js
@@ -1,47 +1,14 @@
-import { useState } from "react"
-
-const History = (props) => {
-  if(props.allClicks.length === 0){
-    return(
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-  return(
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({handleClick, text}) =>(
-  <button onClick = {handleClick}>
-    {text}
-  </button>
-)
-
+import { useState } from 'react'
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleLeftClick = () =>{
-    setAll(allClicks.concat("L"))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () =>{
-    setAll(allClicks.concat("R"))
-    setRight(right + 1)
-  }
-
-  return(
+  return (
     <div>
-      <Button handleClick = {handleLeftClick} text = 'left' />
-      <Button handleClick = {handleRightClick} text = 'right' />
-      <History allClicks = {allClicks} />
+      code here
     </div>
   )
 }
