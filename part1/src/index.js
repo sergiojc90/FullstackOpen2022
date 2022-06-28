267ae16237fd2eda61b41ab902877804aa7c842a
Ricardo <sergiojc90@gmail.com>
2022-06-28 02:08:07 -0500

    Creating startign point for exercises 1.11

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App_1.6_1.14'
+import App from './App_1.12_1.14'
 
 ReactDOM.render(<App />, document.getElementById('root'))
