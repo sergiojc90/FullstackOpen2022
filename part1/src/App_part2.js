b8cbceb6e5cca745643e45dfe906f63c32a2a664
Ricardo <sergiojc90@gmail.com>
2022-06-29 23:47:07 -0500

    Separating components in their own module

--- part1/src/App_part2.js
+++ part1/src/App_part2.js
@@ -1,11 +1,13 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
 
+const App = ({notes}) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+        {notes.map(note =>
+          <Note key = {note.id} note = {note} />
+        )}
       </ul>
     </div>
   )
