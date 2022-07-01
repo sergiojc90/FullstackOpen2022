1d3e3a5ab6c44aa65404cfb12c8810a22641c3a6
Ricardo <sergiojc90@gmail.com>
2022-06-30 23:13:07 -0500

    2.1 - 2.3, refactoring component in to modules

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,5 +1,30 @@
-const Course = () =>{
-    
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Content = ({parts}) => {
+    return(
+        parts.map(part =>
+        <p key ={part.id}>
+            {part.name} {part.exercises}
+        </p>)
+    )
+}
+
+const Total = ({parts}) => 
+    <h4>
+        {"Total of " + 
+        parts.reduce((sum,part) => sum + part.exercises,0) + 
+        " excercises"}
+    </h4>
+
+const Course = ({course}) =>{
+    return(
+        <div>
+            <Header name ={course.name}/>
+            <Content parts ={course.parts}/>
+            <Total parts ={course.parts}/>
+        </div>
+    )
 }
 
 export default Course
