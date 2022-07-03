e2143b5576f5f885c7080a4ed39498dfdda3055c
Ricardo <sergiojc90@gmail.com>
2022-07-03 02:40:03 -0500

    2.8: The Phonebook Step3, add phone number to the object

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,26 +2,41 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas',
+      number: '1234567891'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) =>{
     event.preventDefault()
 
-    if (persons.some(person => person.name===newName))
+    if (newName === '' || newNumber === '')
+    return alert("Name or number can't be empty")
+
+    if(newNumber.length < 10)
+    return alert("Phone number must have 10 digits")
+
+
+    if (persons.some(person => person.name.toUpperCase===newName.toUpperCase))
     return alert(`${newName} is already added to phonebook`)
 
-    const nameObject = {
-      name: newName
+    const personObject = {
+      name: newName,
+      number: newNumber
     }
 
-    setPersons(persons.concat(nameObject))
+    setPersons(persons.concat(personObject))
     setNewName('')
   }
 
-  const handleNamechange = (event) =>{
+  const handleNameChange = (event) =>{
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = event =>{
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -29,15 +44,18 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleNamechange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>   
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <p key ={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key ={person.name}>{person.name}{" "}{person.number}</p>)}
       </ul>
     </div>
   )
