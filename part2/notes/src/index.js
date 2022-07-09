f69e821bdbb5dd482e6a44a0b98df4cc9a47ae87
Ricardo <sergiojc90@gmail.com>
2022-07-09 00:47:42 -0500

    Using useEffect

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -1,13 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/footbar')
-console.log(promise2)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+  <App />)
