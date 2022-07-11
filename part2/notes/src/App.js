011f7a5df1c2120ed473e414e6ed802228f38604
Ricardo <sergiojc90@gmail.com>
2022-07-10 20:08:55 -0500

    creating a post request to the server

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -25,10 +25,15 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
+
+    axios
+      .post('http://localhost:3001/notes',noteObject)
+      .then(reponse =>{
+        console.log(response)
+      })
 
     setNotes(notes.concat(noteObject))
     setNewNote('')
